89cd89ab7c48f36b2ac6515e65c7239fac0ab998
martinnss <martin.olivarest@utem.cl>
2024-04-07 23:58:23 -0400

    header design

--- frontend/src/Containers/HeaderBar.jsx
+++ frontend/src/Containers/HeaderBar.jsx
@@ -5,9 +5,29 @@ import styled from 'styled-components';
 import PrimaryButton from '../Components/PrimaryButton';
 
 
-const Header = styled.header`
+const HeaderDiv = styled.div`
+  position: fixed;
+  height: 8rem;
+  width: 100vw;
+  z-index: 10000; 
+`;
+
+
+const Header = styled.div`
   border-bottom: 1px solid rgb(223, 221, 221);
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  box-shadow: 0 0 40px rgba(0, 0, 0, 0.35);
+  left: 50%; /* Posiciona el div en el centro horizontal */
+  transform: translate(-50%, -50%); /* Centra el div exactamente */
+  position: fixed;
+  top: 4rem; 
+  margin-left: 2rem;
+  margin-right: 2rem;
+  width: 65vw; 
+  height: 5.5rem;
+  z-index: 10000; 
+  border-radius: 5rem;
+  background-color: rgba(255, 255, 255, 0.85);
+  backdrop-filter: blur(15px);
 `;
 
 const HeaderContainer = styled.div`
@@ -58,7 +78,8 @@ const HeaderBar = () => {
   };
 
   return (
-    <Header>
+    <HeaderDiv >
+          <Header>
       <HeaderContainer>
         <LogoContainer>
           <LogoImg src="https://firebasestorage.googleapis.com/v0/b/saving-expenses-tracker.appspot.com/o/logos%2Foutput-onlinepngtools%20(1).png?alt=media&token=3cc14b4a-0b1f-4fd8-8bd1-b54a2861b3ef" alt="walleton logo, a pig" />
@@ -70,6 +91,7 @@ const HeaderBar = () => {
         </LoginContainer>
       </HeaderContainer>
     </Header>
+    </HeaderDiv>
   );
 };
 
